af016d4754a47dfd661608f1611e361a0154096a
mkrasselt1 <github@krasm.de>
2025-04-12 09:55:05 +0200

    Create config.py

---
+++ app/config.py
@@ -0,0 +1,24 @@
+import json
+
+class Config:
+    CONFIG_FILE = "config.json"
+
+    def __init__(self):
+        self.data = {
+            "mqtt_broker": "localhost",
+            "mqtt_port": 1883,
+            "mqtt_topic": "mbus",
+            "mbus_port": "/dev/ttyUSB0"
+        }
+        self.load()
+
+    def load(self):
+        try:
+            with open(self.CONFIG_FILE, "r") as f:
+                self.data = json.load(f)
+        except FileNotFoundError:
+            self.save()
+
+    def save(self):
+        with open(self.CONFIG_FILE, "w") as f:
+            json.dump(self.data, f, indent=4)
