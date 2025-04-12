4d613be2425d3932065117c88bbc9d07f34fd3d8
mkrasselt1 <github@krasm.de>
2025-04-12 10:29:24 +0200

    Update mqtt.py
    add publishing

--- app/mqtt.py
+++ app/mqtt.py
@@ -1,14 +1,18 @@
 import paho.mqtt.client as mqtt
 
+
 class MQTTClient:
-    def __init__(self, broker, port, topic):
+    def __init__(self, broker, port, topic_prefix="mbus"):
         self.broker = broker
         self.port = port
-        self.topic = topic
+        self.topic_prefix = topic_prefix
         self.client = mqtt.Client()
 
     def connect(self):
         self.client.connect(self.broker, self.port)
+        self.client.loop_start()
 
-    def publish(self, payload):
-        self.client.publish(self.topic, payload)
+    def publish(self, topic, payload):
+        full_topic = f"{self.topic_prefix}/{topic}"
+        self.client.publish(full_topic, payload)
+        print(f"Published to topic {full_topic}: {payload}")
