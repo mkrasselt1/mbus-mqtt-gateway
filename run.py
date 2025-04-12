4c1cbbb808fd1dd50cab48a45d2e49e90d0d9330
mkrasselt1 <github@krasm.de>
2025-04-12 09:57:42 +0200

    Create run.py

---
+++ run.py
@@ -0,0 +1,20 @@
+from multiprocessing import Process
+from app.mbus import MBusClient
+from app.mqtt import MQTTClient
+from app.web import app
+from app.config import Config
+import json
+
+def start_mbus_to_mqtt():
+    config = Config()
+    mbus_client = MBusClient(config.data["mbus_port"])
+    mqtt_client = MQTTClient(config.data["mqtt_broker"], config.data["mqtt_port"], config.data["mqtt_topic"])
+    mqtt_client.connect()
+
+    while True:
+        data = mbus_client.read_data()
+        mqtt_client.publish(json.dumps(data))
+
+if __name__ == "__main__":
+    Process(target=start_mbus_to_mqtt).start()
+    app.run(host="0.0.0.0", port=5000)
