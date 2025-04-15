71667073c687cc1879384bed3f5969cb72b72fb7
mkrasselt1 <github@krasm.de>
2025-04-16 00:06:45 +0200

    Update config.py
    add mqtt username and pasword to config

--- app/config.py
+++ app/config.py
@@ -8,10 +8,11 @@ class Config:
             "mqtt_broker": "localhost",
             "mqtt_port": 1883,
             "mqtt_topic": "mbus",
-            "mbus_port": "/dev/ttyUSB0"
+            "mbus_port": "/dev/ttyUSB0",
+            "mqtt_username": "",
+            "mqtt_password": ""
         }
         self.load()
-
     def load(self):
         try:
             with open(self.CONFIG_FILE, "r") as f:
