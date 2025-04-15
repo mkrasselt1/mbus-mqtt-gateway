abe1d2612bc94026a88e57e90699bd8564b8d178
mkrasselt1 <github@krasm.de>
2025-04-16 00:07:21 +0200

    Update mqtt.py
    add mqtt username and password

--- app/mqtt.py
+++ app/mqtt.py
@@ -1,12 +1,16 @@
 import paho.mqtt.client as mqtt
 
-
 class MQTTClient:
-    def __init__(self, broker, port, topic_prefix="mbus"):
+    def __init__(self, broker, port, username=None, password=None, topic_prefix="mbus"):
         self.broker = broker
         self.port = port
+        self.username = username
+        self.password = password
         self.topic_prefix = topic_prefix
         self.client = mqtt.Client()
+
+        if self.username and self.password:
+            self.client.username_pw_set(self.username, self.password)
 
     def connect(self):
         self.client.connect(self.broker, self.port)
